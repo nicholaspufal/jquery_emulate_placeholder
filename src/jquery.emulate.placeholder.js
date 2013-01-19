90501f6ba191e868aeee2562405fb6bd5e3af2e8
Nicholas Pufal <npufal@thoughtworks.com>
2013-01-19 17:45:07 -0200

    Removed comments and added a missing semicolon.

--- src/jquery.emulate.placeholder.js
+++ src/jquery.emulate.placeholder.js
@@ -9,18 +9,15 @@
   $.fn.emulatePlaceholder = function() {
     return this.each(function() {
 
-      //Prevent form from considerating the placeholder as a valid value
       $(this).submit(function() {
         $("input[data-placeholder]").each(function() {
-          $(this).val() == $(this).attr("data-placeholder") && $(this).val("")
+          $(this).val() === $(this).attr("data-placeholder") && $(this).val("");
         });
       });
 
       $("input[data-placeholder]", $(this)).each(function() {
-        //Set data-placeholder as the default value
         $(this).val() === "" && $(this).val($(this).attr("data-placeholder"));
 
-        //Emulate placeholder focus/blur behavior
         $(this)
           .focus(function() {
             $(this).val() === $(this).attr("data-placeholder") && $(this).val("");
